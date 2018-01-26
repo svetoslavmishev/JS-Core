97b4c57d676747a2d5a4e2bbf102401d050bf68f
svetoslavmishev <svetlioo.m@gmail.com>
2018-01-26 08:33:16 +0200

    Add Syntax Operators and Flow

---
+++ JS Fundamentals/03 Syntax Operators and Logic Flow - Exercise/Data Types, Expressions, Statements/2. Area and Perimeter/app.js
@@ -0,0 +1,9 @@
+function areaAndPerimeter(a, b) {
+    let area = a * b;
+    let perimeter = 2 * (a + b);
+
+    console.log(area + `\n` + perimeter);
+}
+areaAndPerimeter(2, 2);
+areaAndPerimeter(1, 3);
+areaAndPerimeter(2.5, 3.14);
---
+++ JS Fundamentals/03 Syntax Operators and Logic Flow - Exercise/Data Types, Expressions, Statements/9. Now Playing/app.js
@@ -0,0 +1,5 @@
+function nowPlaying([artist, track, duration]) {
+
+    console.log(`Now Playing: ${track} - ${artist} [${duration}]`);
+}
+nowPlaying(['Number One', 'Nelly', '4:09']);
